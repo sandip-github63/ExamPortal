454c9f9b797e9cd478e9c38ce2e1a128bf8c5595
sandip <mrsandipkumar63@gmail.com>
2023-10-08 01:06:15 +0530

    Question,Quiz and category added

---
+++ src/main/java/com/exam/model/Category.java
@@ -0,0 +1,81 @@
+package com.exam.model;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "cId")
+	private Long cId;
+
+	@Column(length = 5000)
+	private String title;
+
+	@Column(length = 5000)
+	private String discription;
+
+	@OneToMany(mappedBy = "category", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JsonIgnore
+	private Set<Quiz> quizzes = new LinkedHashSet<>();
+
+	public Set<Quiz> getQuizzes() {
+		return quizzes;
+	}
+
+	public void setQuizzes(Set<Quiz> quizzes) {
+		this.quizzes = quizzes;
+	}
+
+	public Long getcId() {
+		return cId;
+	}
+
+	public void setcId(Long cId) {
+		this.cId = cId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDiscription() {
+		return discription;
+	}
+
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(Long cId, String title, String discription) {
+		super();
+		this.cId = cId;
+		this.title = title;
+		this.discription = discription;
+	}
+
+}
---
+++ src/main/java/com/exam/model/Quiz.java
@@ -0,0 +1,120 @@
+package com.exam.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "quiz")
+public class Quiz {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "quizId")
+	private Long quizId;
+
+	private String title;
+
+	private String description;
+
+	@Column(name = "maxMarks")
+	private int maxMarks;
+
+	@Column(name = "numberOfQuestions")
+	private int numberOfQuestions;
+
+	private boolean active = false;
+
+	@ManyToOne
+	@JoinColumn(name = "cId")
+	private Category category;
+
+	@OneToMany(mappedBy = "quiz", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonIgnore
+	private Set<Question> questions = new HashSet<>();
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Long getQuizId() {
+		return quizId;
+	}
+
+	public void setQuizId(Long quizId) {
+		this.quizId = quizId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getMaxMarks() {
+		return maxMarks;
+	}
+
+	public void setMaxMarks(int maxMarks) {
+		this.maxMarks = maxMarks;
+	}
+
+	public int getNumberOfQuestions() {
+		return numberOfQuestions;
+	}
+
+	public void setNumberOfQuestions(int numberOfQuestions) {
+		this.numberOfQuestions = numberOfQuestions;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Quiz(Long quizId, String title, String description, int maxMarks, int numberOfQuestions, boolean active) {
+		super();
+		this.quizId = quizId;
+		this.title = title;
+		this.description = description;
+		this.maxMarks = maxMarks;
+		this.numberOfQuestions = numberOfQuestions;
+		this.active = active;
+	}
+
+	public Quiz() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
